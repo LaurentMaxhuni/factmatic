680853dbe337c515195997414b8a54d0ad454bbb
LaurentMaxhuni <152623037+LaurentMaxhuni@users.noreply.github.com>
2025-07-18 12:06:09 +0200

    Battery low... :(

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='container'>
+        <img src='/logo.png' className='logo'></img>
+        <h1>Fact Matic</h1>
+        <p>Get your daily facts from Fact Matic.</p>
+        <ul className='links-list'>
+          <li className='links-item'><a className='links-link'>Instagram</a></li>
+          <li className='links-item'><a className='links-link'>TikTok</a></li>
+          <li className='links-item'><a className='links-link'>Gumroad</a></li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default App
