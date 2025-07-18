da30e8bd74960490bed8b00942ceb3eb6c76589f
Laurent Maxhuni <laurentmaxhuni1234@gmail.com>
2025-07-18 16:04:31 +0200

    idk

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,24 @@ import './App.css'
 function App() {
   return (
     <>
-      <div className='container'>
-        <img src='/logo.png' className='logo'></img>
-        <h1>Fact Matic</h1>
-        <p>Get your daily facts from Fact Matic.</p>
-        <ul className='links-list'>
-          <li className='links-item'><a className='links-link'>Instagram</a></li>
-          <li className='links-item'><a className='links-link'>TikTok</a></li>
-          <li className='links-item'><a className='links-link'>Gumroad</a></li>
-        </ul>
+      <div className="container">
+        <img src="/logo.png" className="logo"></img>
+        <h1 className="title">Fact Matic</h1>
+        <p className="info">Get your daily facts from Fact Matic.</p>
+        <div className="button-container">
+          <button>
+            <a href="">Check out my TikTok!</a>
+          </button>
+          <button>
+            <a href="">Check out my Instagram!</a>
+          </button>
+          <button>
+            <a href="">Buy my 30 days, 30 facts book!</a>
+          </button>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
 export default App
